52f75db557a3eab4978fbf08963407c6b9d660fc
Poipurin <b.pinorojo@gmail.com>
2023-04-18 00:20:52 -0400

    se añade validación

--- 9.1Validacion_Formulario/scripts.js
+++ 9.1Validacion_Formulario/scripts.js
@@ -1,19 +1,75 @@
-const name = document.getElementById("name");
-const invalidName = document.getElementById("invalidName");
+import './styles.css';
+
+const form = document.getElementById("form");
+const userName = document.getElementById("name");
+const lastName = document.getElementById("lastname");
 const email = document.getElementById("email");
 const pass = document.getElementById("pass");
-const invalidPass = document.getElementById("");
+const passConf = document.getElementById("pass2");
 
 
-function nameExtension(nameValue) {
-    if (nameValue.length >= 3 && nameValue < 13) {
-        return true;
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    checkInputs();
+});
+
+function checkInputs() {
+    const userNameValue = userName.value.trim();
+    const lastNameValue = lastName.value.trim();
+    const emailValue = email.value.trim();
+    const passValue = pass.value.trim();
+    const passConfValue = passConf.value.trim();
+
+    if (userNameValue === " ") {
+        setErrorFor(userName, "El campo no puede estar vacío");
     } else {
-        invalidName
-    };
+        setSuccessFor(userName);
+    }
+
+    if (lastNameValue === " ") {
+        setErrorFor(lastName, "El campo no puede estar vacío");
+    } else {
+        setSuccessFor(lastName);
+    }
+
+    if (emailValue === " ") {
+        setErrorFor(email, "El campo no puede estar vacío");
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, "El email no es válido");
+    } else {
+        setSuccessFor(email);
+    }
+
+    if (passValue === " ") {
+        setErrorFor(pass, "El campo no puede estar vacío");
+    } else {
+        setSuccessFor(pass);
+    }
+
+    if (passConfValue === " ") {
+        setErrorFor(passConf, "El campo no puede estar vacío");
+    } else if (passConf !== passConfValue) {
+        setErrorFor(passConf, "Las contraseñas no coinciden");
+    } else {
+        setSuccessFor(passConf);
+    }
 }
 
-function email(){
-
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector("small");
+    formControl.className = "form-control error";
+    small.innerText = message;
 }
 
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+    formControl.className = "form-control success";
+}
+
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        email
+    );
+}
